ac96ec40f3fa5a1559763943f2f2292b2833aee0
Doppalapudi Madhuri <apple@Doppalapudi-Madhuri.local>
2019-09-28 17:44:28 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyCOnhfOz4A4sy6dP71TvzU9-li-IbE-QzM',
+    authDomain: 'first-f4b80.firebaseapp.com',
+    databaseURL: 'https://first-f4b80.firebaseio.com',
+    projectId: 'first-f4b80',
+    storageBucket: 'first-f4b80.appspot.com',
+    messagingSenderId: '301925653730',
+    appId: '1:301925653730:web:a4d763ffdc2da6809312bb'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyCOnhfOz4A4sy6dP71TvzU9-li-IbE-QzM',
+    authDomain: 'first-f4b80.firebaseapp.com',
+    databaseURL: 'https://first-f4b80.firebaseio.com',
+    projectId: 'first-f4b80',
+    storageBucket: 'first-f4b80.appspot.com',
+    messagingSenderId: '301925653730',
+    appId: '1:301925653730:web:a4d763ffdc2da6809312bb'
+  }
 };
 
 /*
